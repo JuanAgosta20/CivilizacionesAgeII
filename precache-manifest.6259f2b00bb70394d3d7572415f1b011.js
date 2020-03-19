8cdcb4be4044262e5851cdbcab6fe8e3162e429d
JuanAgosta20 <50495331+JuanAgosta20@users.noreply.github.com>
2020-03-18 21:19:19 -0300

    Updates

---
+++ precache-manifest.6259f2b00bb70394d3d7572415f1b011.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b76e205ec919cf64a7434497a85974",
+    "url": "/CivilizacionesAgeII/index.html"
+  },
+  {
+    "revision": "9cca40a19038e8a751ef",
+    "url": "/CivilizacionesAgeII/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "75282b1f46a61f05792d",
+    "url": "/CivilizacionesAgeII/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9cca40a19038e8a751ef",
+    "url": "/CivilizacionesAgeII/static/js/2.92c7d9e1.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/CivilizacionesAgeII/static/js/2.92c7d9e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75282b1f46a61f05792d",
+    "url": "/CivilizacionesAgeII/static/js/main.88c67479.chunk.js"
+  },
+  {
+    "revision": "64a846d8fdc554e1b83f",
+    "url": "/CivilizacionesAgeII/static/js/runtime-main.0d49fc9d.js"
+  },
+  {
+    "revision": "4c3adbdf94636dff2cb7591229ee970d",
+    "url": "/CivilizacionesAgeII/static/media/1.4c3adbdf.png"
+  },
+  {
+    "revision": "249592ea5bf7347dfa7e112b8938f216",
+    "url": "/CivilizacionesAgeII/static/media/10.249592ea.png"
+  },
+  {
+    "revision": "8fdc0be39e5a0b851fe90d46719e2ace",
+    "url": "/CivilizacionesAgeII/static/media/11.8fdc0be3.png"
+  },
+  {
+    "revision": "29f62dec8b0957ded5aa3380ebd813ed",
+    "url": "/CivilizacionesAgeII/static/media/12.29f62dec.png"
+  },
+  {
+    "revision": "131026f6fb85169d62ab6320423091e9",
+    "url": "/CivilizacionesAgeII/static/media/13.131026f6.png"
+  },
+  {
+    "revision": "0626d29246cd929e1d35c1b764bd8fa0",
+    "url": "/CivilizacionesAgeII/static/media/14.0626d292.png"
+  },
+  {
+    "revision": "a439ffbbc26c4cbecdd724dc010b96b5",
+    "url": "/CivilizacionesAgeII/static/media/15.a439ffbb.png"
+  },
+  {
+    "revision": "4dd8373df75ba5cbd272b6d4f7ead5f4",
+    "url": "/CivilizacionesAgeII/static/media/16.4dd8373d.png"
+  },
+  {
+    "revision": "69349b30654ff11aa5813d5211ad75b2",
+    "url": "/CivilizacionesAgeII/static/media/17.69349b30.png"
+  },
+  {
+    "revision": "e7ba8cf91365e1b24c7e215c13cba2b9",
+    "url": "/CivilizacionesAgeII/static/media/18.e7ba8cf9.png"
+  },
+  {
+    "revision": "5fce35c92fcadb0e6c3251d7f92e6724",
+    "url": "/CivilizacionesAgeII/static/media/2.5fce35c9.png"
+  },
+  {
+    "revision": "e1df6de64bed06f586df25775e692f91",
+    "url": "/CivilizacionesAgeII/static/media/3.e1df6de6.png"
+  },
+  {
+    "revision": "545debed9e199e92065aa960770be6c0",
+    "url": "/CivilizacionesAgeII/static/media/4.545debed.png"
+  },
+  {
+    "revision": "cf91ed387100fd04994578796e3aab3a",
+    "url": "/CivilizacionesAgeII/static/media/5.cf91ed38.png"
+  },
+  {
+    "revision": "db66085541d3c5f7805a7ec790a7c9a8",
+    "url": "/CivilizacionesAgeII/static/media/6.db660855.png"
+  },
+  {
+    "revision": "70838d5014a5e4770f77f42de2f5a520",
+    "url": "/CivilizacionesAgeII/static/media/7.70838d50.png"
+  },
+  {
+    "revision": "94e925195747d675665544ccd8629dea",
+    "url": "/CivilizacionesAgeII/static/media/8.94e92519.png"
+  },
+  {
+    "revision": "048f47e3e4fc246f8ce7101b288e7e6e",
+    "url": "/CivilizacionesAgeII/static/media/9.048f47e3.png"
+  },
+  {
+    "revision": "a1fbe66b4f49cac6a7a4d33c92f5ad10",
+    "url": "/CivilizacionesAgeII/static/media/Bizantinos.a1fbe66b.jpg"
+  },
+  {
+    "revision": "3132fc91039232d656a85024fa3f6b5b",
+    "url": "/CivilizacionesAgeII/static/media/Britanicos.3132fc91.jpg"
+  },
+  {
+    "revision": "e5e8487347346f50751bd013ba5b4102",
+    "url": "/CivilizacionesAgeII/static/media/Españoles.e5e84873.jpg"
+  },
+  {
+    "revision": "1cd4526f5684483cbdb88e0c7812d5b4",
+    "url": "/CivilizacionesAgeII/static/media/Francos.1cd4526f.jpg"
+  },
+  {
+    "revision": "e12d8360cb728da57c96e5d39b6065eb",
+    "url": "/CivilizacionesAgeII/static/media/Mongoles.e12d8360.jpg"
+  },
+  {
+    "revision": "fc1b9dd978b6463842829c57c04ed156",
+    "url": "/CivilizacionesAgeII/static/media/Persia.fc1b9dd9.jpg"
+  },
+  {
+    "revision": "8a7c2b3805f1aeff7ce9bf58a67dba9d",
+    "url": "/CivilizacionesAgeII/static/media/Sarracenos.8a7c2b38.jpg"
+  },
+  {
+    "revision": "9067d73ad19d6b95d7208694156b8ce2",
+    "url": "/CivilizacionesAgeII/static/media/Turcos.9067d73a.jpg"
+  },
+  {
+    "revision": "70138a2cf9b88713f821399720444d2d",
+    "url": "/CivilizacionesAgeII/static/media/aztecas.70138a2c.jpg"
+  },
+  {
+    "revision": "3e28965d92e3a15ab4600bfce253aa23",
+    "url": "/CivilizacionesAgeII/static/media/celtas.3e28965d.jpg"
+  },
+  {
+    "revision": "c3109155dbc17a45ab3bf163c76c32fd",
+    "url": "/CivilizacionesAgeII/static/media/chinos.c3109155.jpg"
+  },
+  {
+    "revision": "c654afe3432555f4758d94461e81f116",
+    "url": "/CivilizacionesAgeII/static/media/godosa.c654afe3.jpg"
+  },
+  {
+    "revision": "d09ca2ca80fd126410b6770b56713a63",
+    "url": "/CivilizacionesAgeII/static/media/hunos.d09ca2ca.jpg"
+  },
+  {
+    "revision": "579745c44370bdf0a67b4afe7fe8fd1a",
+    "url": "/CivilizacionesAgeII/static/media/japoneses.579745c4.jpg"
+  },
+  {
+    "revision": "da763e8f0bffa3ba3a344c2c8fffb3b6",
+    "url": "/CivilizacionesAgeII/static/media/korea.da763e8f.jpg"
+  },
+  {
+    "revision": "4c6a89f5aab79ce17b8dba4d4c8b8c72",
+    "url": "/CivilizacionesAgeII/static/media/mayas.4c6a89f5.jpg"
+  },
+  {
+    "revision": "a28a6480a5bf26d7200164933198637e",
+    "url": "/CivilizacionesAgeII/static/media/teutones.a28a6480.jpg"
+  },
+  {
+    "revision": "a1e2a49f3f774044001a297826b54ce3",
+    "url": "/CivilizacionesAgeII/static/media/vikingos.a1e2a49f.jpg"
+  }
+]);
